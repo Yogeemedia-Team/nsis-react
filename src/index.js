a20efb53bd2c9f796a5430386800a2541c464d88
Kasun24 <kasun961224@gmail.com>
2023-12-19 16:39:56 +0530

    path changed

--- src/index.js
+++ src/index.js
@@ -24,10 +24,10 @@ import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
 
 import Index from "views/Index.js";
-import Landing from "views/examples/Landing.js";
-import Login from "views/examples/Login.js";
-import Profile from "views/examples/Profile.js";
-import Register from "views/examples/Register.js";
+import Landing from "views/pages/Landing.js";
+import Login from "views/pages/Login.js";
+import Profile from "views/pages/Profile.js";
+import Register from "views/pages/Register.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
